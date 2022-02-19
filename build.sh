5b2567002848f7d9d2080561fdb2008885a3d90a
0xb00bface <0xboobface@gmail.com>
2022-02-19 12:44:06 +0100

    Update electron to version 17.0.1

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf ctbrec-minimal-browser*x64
-node node_modules/electron-packager/cli.js --overwrite . ctbrec-minimal-browser --platform=linux --arch=x64 --icon icon.png
+npx electron-packager --overwrite . ctbrec-minimal-browser --platform=linux --arch=x64 --icon icon.png
 # windows app has to be build on windows or wine must be installed
-#node node_modules/electron-packager/cli.js --overwrite . ctbrec-minimal-browser --platform=win32 --arch=x64
-node node_modules/electron-packager/cli.js --overwrite . ctbrec-minimal-browser --platform=darwin --arch=x64 --icon icon.icns 
+#npx electron-packager --overwrite . ctbrec-minimal-browser --platform=win32 --arch=x64 --icon icon.ico
+npx electron-packager --overwrite . ctbrec-minimal-browser --platform=darwin --arch=x64 --icon icon.icns
